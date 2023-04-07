1104cd494e2c71c07f2d6a0bea29de72a14d4720
GAsNa <romane.leseur.pro@gmail.com>
2023-04-07 16:42:05 +0200

    connection WiFi compil ok

--- motion_detector.ino
+++ motion_detector.ino
@@ -1,3 +1,5 @@
+#include <WiFi.h>
+
 // include env.h with define:
 //    - SSID_NAME
 //    - SSID_PASS
@@ -11,6 +13,23 @@ int       pinStatePrevious  = LOW;
 void setup() {
 
   Serial.begin(9600);                                           // Initialize serial, bps
+  
+  delay(1000);                                                  // Why delay here ?
+
+  WiFi.mode(WIFI_STA);                                          // Optionnal
+  WiFi.begin(SSID_NAME, SSID_PASS);                             // Set credentials for WiFi connection
+  Serial.println("\nConnecting");
+
+  while (WiFi.status() != WL_CONNECTED)                         // While not connected to WiFi, try again
+  {
+    Serial.print(".");
+    delay(100);
+  }
+
+  Serial.println("\nConnected to the WiFi network");
+  Serial.print("Local ESP32 IP: ");
+  Serial.println(WiFi.localIP());                               // Print local ESP32 IP
+
   pinMode(PIN_TO_SENSOR, INPUT);                                // Set ESP32 pin to input mode to read value from OUTPUT pin of sensor
 
 }
